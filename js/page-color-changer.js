5ee2be55c84b7984a5e990fdd43e71756e7488b2
PranviJeria <pranvijeria@gmail.com>
2025-06-19 22:51:34 -0400

    added constants for RBG sliders and body elements

---
+++ js/page-color-changer.js
@@ -0,0 +1,6 @@
+//get slider elements and body
+const red= document.getElementById('red');
+const green= document.getElementById('green');
+const blue= document.getElementById('blue');
+const body = document.body;
+
